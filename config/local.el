799868a88ccec87225f07c6df60ebfb4c7d70755
Ponkore <ponkore@me.com>
2012-09-03 11:13:43 +0900

    clojure-mode の時だけ、eldoc-mode は自動で on にしない。

--- config/local.el
+++ config/local.el
@@ -143,7 +143,7 @@
 (setq lisp-hook-fn (lambda () (paredit-mode) (yas-minor-mode) (eldoc-mode)))
 (add-hook 'lisp-mode-hook lisp-hook-fn)
 (add-hook 'emacs-lisp-mode-hook lisp-hook-fn)
-(add-hook 'clojure-mode-hook lisp-hook-fn)
+(add-hook 'clojure-mode-hook (lambda () (paredit-mode) (yas-minor-mode)))
 
 
 ;;; ac-nrepl
