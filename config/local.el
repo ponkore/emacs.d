72f17a4ddb5bb62273cc205df3348e8e90ee4e11
Ponkore <ponkore@me.com>
2012-09-06 21:20:04 +0900

    customize hl-line face (underline style)

--- config/local.el
+++ config/local.el
@@ -70,6 +70,8 @@
 ;;; 行番号の表示
 (global-linum-mode t)      ; デフォルトで linum-mode を有効にする
 (setq linum-format "%5d ") ; 5 桁分の領域を確保して行番号のあとにスペースを入れる
+;;; 選択行のハイライト表示の face
+(custom-set-faces '(hl-line ((t (:underline "textBackgroundColor")))))
 
 
 ;;; clojure-mode
