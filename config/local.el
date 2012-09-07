468b659b3889e0a638b61ed413d7bf15d730e604
Ponkore <ponkore@me.com>
2012-09-07 21:39:44 +0900

    Japanese font setting moved to functions/local.el, and etc...

--- config/local.el
+++ config/local.el
@@ -32,29 +32,7 @@
 ;;; Mac でフォントを変えてみる
 ;;;
 (when (and (eq window-system 'ns) (>= emacs-major-version 23))
-  (set-face-attribute 'default nil
-                      :family "monaco"
-                      :height 140)
-  (set-fontset-font
-   (frame-parameter nil 'font)
-   'japanese-jisx0208
-   '("Hiragino Maru Gothic Pro" . "iso10646-1"))
-  (set-fontset-font
-   (frame-parameter nil 'font)
-   'japanese-jisx0212
-   '("Hiragino Maru Gothic Pro" . "iso10646-1"))
-  (set-fontset-font
-   (frame-parameter nil 'font)
-   'mule-unicode-0100-24ff
-   '("monaco" . "iso10646-1"))
-  (setq face-font-rescale-alist
-        '(("^-apple-hiragino.*" . 1.2)
-          (".*osaka-bold.*" . 1.2)
-          (".*osaka-medium.*" . 1.2)
-          (".*courier-bold-.*-mac-roman" . 1.0)
-          (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
-          (".*monaco-bold-.*-mac-roman" . 0.9)
-          ("-cdac$" . 1.3))))
+  (cocoa-emacs-font-setting 12))
 
 
 ;;; modeline に column-number は表示しない
