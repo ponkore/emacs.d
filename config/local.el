7bcbe13c5dce8f0f4a3348cc0316bef955b44f3f
Ponkore <ponkore@me.com>
2012-09-12 10:39:46 +0900

    add compojure keyword for indentation

--- config/local.el
+++ config/local.el
@@ -57,7 +57,18 @@
 ;;
 (el-get 'sync '(clojure-mode))
 (el-get 'sync '(nrepl))
+;;; for compojure
+(require 'clojure-mode)
 
+(define-clojure-indent
+  (defroutes 'defun)
+  (GET 2)
+  (POST 2)
+  (PUT 2)
+  (DELETE 2)
+  (HEAD 2)
+  (ANY 2)
+  (context 2))
 
 ;;; color-theme
 ;;
