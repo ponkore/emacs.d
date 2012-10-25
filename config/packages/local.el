01512fc261aa63e3aca9df0e41d3efc1d91f0371
Masao KATO <kato.masao@osk.ns-sol.co.jp>
2012-10-25 16:18:40 +0900

    modify for visual-basic-mode

--- config/packages/local.el
+++ config/packages/local.el
@@ -1,6 +1,7 @@
 ;;
 ;; Visual Basic Mode - visual-basic-mode.el
 ;;
+(add-to-list 'load-path "~/.emacs.d/config/packages")
 (autoload 'visual-basic-mode "visual-basic-mode" "Visual Basic mode." t)
 (setq visual-basic-mode-indent 4)
 (setq auto-mode-alist (append '(("\\.\\(frm\\|bas\\|cls\\|vbs\\|vb\\)$" . 
