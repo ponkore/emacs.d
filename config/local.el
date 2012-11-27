cd2d73defd04687e0d761a04ab053165e7bfbc41
ponkore <ponkore@gmail.com>
2012-11-27 15:58:39 +0900

    add gtags and key bindings

--- config/local.el
+++ config/local.el
@@ -216,6 +216,18 @@
 (global-set-key (kbd "C-=") 'er/expand-region)
 
 
+;;; gtags
+;;
+;;
+(el-get 'sync '(gtags))
+(require 'gtags)
+(global-set-key "\M-." 'gtags-find-tag)     ;関数の定義元へ
+(global-set-key "\M-," 'gtags-find-rtag)    ;関数の参照先へ
+(global-set-key "\M-s" 'gtags-find-symbol)  ;変数の定義元/参照先へ
+(global-set-key "\M-f" 'gtags-find-file)    ;ファイルにジャンプ
+(global-set-key "\C-t" 'gtags-pop-stack)   ;前のバッファに戻る
+
+
 ;;; for SQL mode (My Office PC Oracle setting)
 (when (eq system-type 'windows-nt)
   (setq sql-oracle-program "c:/Apps/Oracle/instantclient_11_2/sqlplus.exe"))
