f3fb54e53badc37f49af774900543b7f7edb7f22
Masao KATO <kato.masao@osk.ns-sol.co.jp>
2013-01-28 11:02:18 +0900

    fixed open directory as external command (on windows).

--- config/local.el
+++ config/local.el
@@ -21,7 +21,7 @@
 
 
 ;; Windows/Mac dired quick hack: open any documents with external command.
-(defvar open-directory-command "explorer.exe" "Open a directory with suitable windows/mac command.")
+(defvar open-directory-command "cygstart.exe" "Open a directory with suitable windows/mac command.")
 (defvar open-file-command "cygstart.exe" "Open a file with suitable windows/mac command.")
 (defun dired-open-external ()
   "Open current line of dired buffer with external (windows) command."
