e6b5c51dce6053c9196a175a3ad256709057de65
Ponkore <ponkore@me.com>
2013-02-22 22:28:50 +0900

    some generic function moved to functions/local.el

--- config/packages/helm-hosen.el
+++ config/packages/helm-hosen.el
@@ -1,47 +1,44 @@
+;;
 ;; helm-source for developing
-(defun read-file-and-list-each-lines (filename)
-  (save-excursion
-    (let* ((buffer (find-file-noselect filename))
-           (ret nil))
-      (set-buffer buffer)
-      (goto-char (point-min))
-      (while (re-search-forward "^.+$" nil t)
-        (let* ((beg (match-beginning 0))
-               (end (match-end 0)))
-          (setq ret (append ret (list (list (buffer-substring beg end)))))))
-      (kill-buffer buffer)
-      ret)))
-
-(defun make-helm-source-from-file (filename source-name)
-  (let ((l (read-file-and-list-each-lines filename)))
+;;
+(defun make-helm-source-from-file (source-name filename)
+  (let ((l (mapcar (lambda (v) (list v)) (read-file-and-list-each-lines filename))))
     `((name . ,source-name)
       (candidates . ,l)
       (action . message))))
 
-(defvar helm-root-user-zai-source
-  (make-helm-source-from-file "C:/repo/hosen-tools/helm-root-user-zai.txt"
-                              "rootユーザ(在)"))
+(defvar helm-hosen-configured nil)
+(defvar helm-root-user-zai-source nil)
+(defvar helm-root-user-kan-source nil)
+(defvar helm-senmei-source nil)
+(defvar helm-eki-zai-source nil)
+(defvar helm-kanrishitsu-eki-zai-source nil)
 
-(defvar helm-root-user-kan-source
-  (make-helm-source-from-file "C:/repo/hosen-tools/helm-root-user-kan.txt"
-                              "rootユーザ(幹)"))
-
-(defvar helm-senmei-source
-  (make-helm-source-from-file "C:/repo/hosen-tools/helm-線名.txt"
-                              "線名(在)"))
-
-(defvar helm-eki-zai-source
-  (make-helm-source-from-file "C:/repo/hosen-tools/helm-駅.txt"
-                              "駅(在)"))
-
-(defvar helm-kanrishitsu-eki-zai-source
-  (make-helm-source-from-file "C:/repo/hosen-tools/helm-管理室駅.txt"
-                              "管理室＿駅(在)"))
+(defun helm-hosen-config ()
+  (interactive)
+  (unless helm-hosen-configured
+    (setq helm-root-user-zai-source
+          (make-helm-source-from-file "rootユーザ(在)"
+                                      "C:/repo/hosen-tools/helm-root-user-zai.txt"))
+    (setq helm-root-user-kan-source
+          (make-helm-source-from-file "rootユーザ(幹)"
+                              "C:/repo/hosen-tools/helm-root-user-kan.txt"))
+    (setq helm-senmei-source
+          (make-helm-source-from-file "線名(在)"
+                                      "C:/repo/hosen-tools/helm-線名.txt"))
+    (setq helm-eki-zai-source
+          (make-helm-source-from-file "駅(在)"
+                                      "C:/repo/hosen-tools/helm-駅.txt"))
+    (setq helm-kanrishitsu-eki-zai-source
+          (make-helm-source-from-file "管理室＿駅(在)"
+                              "C:/repo/hosen-tools/helm-管理室駅.txt"))
+    (setq helm-hosen-configured t)))
 
 (defun helm-root-user ()
   (interactive)
   (helm-other-buffer
-   '(helm-root-user-zai-source helm-root-user-kan-source) "*helm root user*"))
+   '(helm-root-user-zai-source helm-root-user-kan-source)
+   "*helm root user*"))
 
 (defun helm-hosen-zai ()
   (interactive)
@@ -50,3 +47,5 @@
      helm-eki-zai-source
      helm-kanrishitsu-eki-zai-source)
    "*helm zai*"))
+
+(helm-hosen-config)
