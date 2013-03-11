0088be14e8f72c3d87edd5355c400135a6e2a1bc
Ponkore <ponkore@me.com>
2013-03-11 22:49:39 +0900

    nrepl & ac-nrepl improved.

--- config/local.el
+++ config/local.el
@@ -193,8 +193,17 @@
 (require 'ac-nrepl)
 ;; nREPL バッファも auto-complete したいので以下の hook を入れる
 (add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
-(add-hook 'clojure-nrepl-mode-hook 'ac-nrepl-setup)
+(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
+;(add-hook 'clojure-nrepl-mode-hook 'ac-nrepl-setup)
 (eval-after-load "auto-complete" '(add-to-list 'ac-modes 'nrepl-mode))
+
+(defun set-auto-complete-as-completion-at-point-function ()
+  (setq completion-at-point-functions '(auto-complete)))
+(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+
+(add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
 
 
 ;;; SLIME and ac-slime
