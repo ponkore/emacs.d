dc4fa1397f6f67e443618cc5535bab827081796e
Masao KATO <ponkore@gmail.com>
2013-11-19 17:01:16 +0900

    nrepl -> CIDER

--- config/local.el
+++ config/local.el
@@ -107,9 +107,9 @@
 
 
 ;;; clojure-mode
-;; clojure 編集用のモード & nREPL インタフェース
+;; clojure 編集用のモード & CIDER
 ;;
-(el-get 'sync '(clojure-mode nrepl midje-mode))
+(el-get 'sync '(clojure-mode cider midje-mode))
 ;;; for compojure indent
 (require 'clojure-mode)
 (require 'midje-mode)
@@ -125,8 +125,19 @@
   (HEAD 2)
   (ANY 2)
   (context 2))
-;;; easy to use (nrepl-jack-in)
-(define-key clojure-mode-map (kbd "C-c M-j") 'nrepl-jack-in)
+;;; easy to use (cider-jack-in)
+(define-key clojure-mode-map (kbd "C-c M-j") 'cider-jack-in)
+
+;;; when windows, remove ^M in nrepl buffer
+;;; thanx http://takeisamemo.blogspot.jp/2013/10/clojureemacs-windowsnreplm.html
+(when (eq system-type 'windows-nt)
+  (defun remove-dos-eol ()
+    "Do not show ^M in files containing mixed UNIX and DOS line endings."
+    (interactive)
+    (setq buffer-display-table (make-display-table))
+    (aset buffer-display-table ?\^M []))
+  (add-hook 'cider-mode-hook 'remove-dos-eol))
+
 
 ;;; color-theme
 ;;
@@ -201,17 +212,16 @@
 (el-get 'sync '(ac-nrepl))
 (require 'ac-nrepl)
 ;; nREPL バッファも auto-complete したいので以下の hook を入れる
-(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
-(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
-(eval-after-load "auto-complete" '(add-to-list 'ac-modes 'nrepl-mode))
+(add-hook 'cider-mode-hook 'ac-nrepl-setup)
+(eval-after-load "auto-complete" '(add-to-list 'ac-modes 'cider-mode))
 
 (defun set-auto-complete-as-completion-at-point-function ()
   (setq completion-at-point-functions '(auto-complete)))
 (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
 
-(add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
-(add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
-(define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
+(add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'cider-repl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(define-key cider-repl-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
 
 
 ;;; SLIME and ac-slime
