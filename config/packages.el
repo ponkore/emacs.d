f4521fd1435c15e46b8691bbb455963545e4b0cc
Ponkore <ponkore@me.com>
2013-12-11 22:31:50 +0900

    old configration removed.

--- config/packages.el
+++
@@ -1,136 +0,0 @@
-;;;
-(if (eq system-type 'windows-nt)
-    (load "config/builtins/gnupack-init"))
-;;; el-get
-;; 複数のソースからパッケージをインストールできるパッケージ管理システム
-;; 2012-03-15
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-(unless (require 'el-get nil t)
-  ;;; @ponkore, force master branch.
-  (setq el-get-install-branch "master")
-  ;;;
-  ;; (with-current-buffer
-  ;;     (url-retrieve-synchronously
-  ;;      "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-  ;;   (end-of-buffer)
-  ;;   (eval-print-last-sexp))
-  (load "~/.emacs.d/el-get-install.el")
-  )
-;; レシピ置き場
-(add-to-list 'el-get-recipe-path
-             (concat (file-name-directory load-file-name) "/el-get/recipes"))
-;; 追加のレシピ置き場
-(add-to-list 'el-get-recipe-path
-             "~/.emacs.d/config/el-get/local-recipes")
-
-
-;;; grep-edit
-;; *grep*で編集できるようにする
-(el-get 'sync '(grep-edit))
-(add-hook 'grep-setup-hook
-          (lambda ()
-            (define-key grep-mode-map
-              (kbd "C-c C-c") 'grep-edit-finish-edit)))
-
-
-;;; ポップアップ
-;; 2012-03-16
-(el-get 'sync '(popup))
-
-
-;;; Auto Complete
-;; 自動補完
-(el-get 'sync '(auto-complete))
-(add-hook 'auto-complete-mode-hook
-          (lambda ()
-            (define-key ac-completing-map (kbd "C-n") 'ac-next)
-            (define-key ac-completing-map (kbd "C-p") 'ac-previous)))
-
-
-;;; Helm
-;; iswitchbの代わり
-(let ((original-browse-url-browser-function browse-url-browser-function))
-  (el-get 'sync '(helm))
-  (require 'helm-config)
-;   (anything-set-anything-command-map-prefix-key 'anything-command-map-prefix-key "C-c C-<SPC>")
-  (global-set-key (kbd "C-c h") 'helm-mini)
-  (helm-mode)
-  (define-key global-map (kbd "C-x b") 'helm-for-files)
-  (define-key global-map (kbd "C-x C-b") 'helm-for-files)
-  (define-key global-map (kbd "M-y") 'helm-show-kill-ring)
-  (define-key helm-map (kbd "C-z") 'helm-previous-page)
-  (define-key helm-map (kbd "C-l") 'helm-execute-persistent-action)
-  (define-key helm-map (kbd "C-o") nil)
-  (define-key helm-map (kbd "C-M-n") 'helm-next-source)
-  (define-key helm-map (kbd "C-M-p") 'helm-previous-source)
-  (setq browse-url-browser-function original-browse-url-browser-function))
-
-
-;;; Migemo
-;; ローマ字で日本語をインクリメンタルサーチする。
-;; 2012-03-19
-;; インストールされていたら有効にする。
-;(require 'migemo nil t)
-
-
-;;; rabbit-mode
-;; 2012-03-16
-;(el-get 'sync '(rabbit-mode))
-
-
-;;; run-test
-;; テスト実行
-(el-get 'sync '(run-test))
-
-
-;;; Magit
-;; 2012-03-24
-(load "config/packages/magit")
-
-
-;;; rst-mode
-;; reStructuredText編集用のモード
-;; 2012-03-24
-(el-get 'sync '(rst-mode))
-
-
-;;; textile-mode
-;; Textile編集用のモード
-;; 2012-04-11
-(el-get 'sync '(textile-mode))
-
-
-;;; js2-mode
-;; 2012-04-05
-(load "config/packages/js2-mode")
-
-
-;;; coffe-mode
-;; CoffeeScript編集用のモード
-;; 2012-04-04
-(el-get 'sync '(coffee-mode))
-
-
-;;; less-css-mode
-;; LESS編集用のモード
-;; 2012-09-23
-(el-get 'sync '(less-css-mode))
-
-
-;;; Milkode
-;; ソースコード検索エンジン
-;; 2012-09-28
-(el-get 'sync '(milkode))
-
-
-;;; markdown-mode
-;; Markdown編集用のモード
-;; 2012-10-15
-(el-get 'sync '(markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.\\(markdown\\|md\\)\\.txt\\'" . markdown-mode))
-
-
-;;; 追加の設定
-;; 個別の設定があったら読み込む
-;; 2012-03-15
-(load "config/packages/local" t)
--- config/packages/local.el
+++
@@ -1,16 +0,0 @@
-;;
-;; Visual Basic Mode - visual-basic-mode.el
-;;
-(add-to-list 'load-path "~/.emacs.d/config/packages")
-(autoload 'visual-basic-mode "visual-basic-mode" "Visual Basic mode." t)
-(setq visual-basic-mode-indent 4)
-(setq auto-mode-alist (append '(("\\.\\(frm\\|bas\\|cls\\|vbs\\|vb\\)$" . 
-                                visual-basic-mode)) auto-mode-alist))
-(add-hook 'visual-basic-mode-hook
-          '(lambda () (setq mode-name "vb")))
-
-
-;;; psvn-mode
-;;
-;; 2012-10-25
-(el-get 'sync '(psvn))
