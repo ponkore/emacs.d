73c73c4a895c78159429bfd55d67721838b3a1f5
Masao KATO <ponkore@gmail.com>
2014-02-20 09:30:22 +0900

    invalidate color-theme with windows/gnupack/emacs.

---
+++ site-lisp/color-theme.el
@@ -0,0 +1,8 @@
+;; invalidate gnupack/app/emacs/site-lisp/color-theme
+(defun color-theme-initialize ()
+  (interactive)
+  nil)
+(defun color-theme-andreas ()
+  (interactive)
+  nil)
+(provide 'color-theme)
