a1d562351da3e1ed927dccc4d3083ea54482a651
Masao KATO <ponkore@gmail.com>
2014-05-08 19:19:29 +0900

    paredit-mode on when cider-repl started

--- inits/50_paredit.el
+++ inits/50_paredit.el
@@ -5,4 +5,3 @@
 (setq lisp-hook-fn (lambda () (paredit-mode) (yas-minor-mode) (eldoc-mode)))
 (add-hook 'lisp-mode-hook lisp-hook-fn)
 (add-hook 'emacs-lisp-mode-hook lisp-hook-fn)
-(add-hook 'clojure-mode-hook (lambda () (paredit-mode) (yas-minor-mode)))
