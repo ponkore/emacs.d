ca1a981d302203ed1fb2e83d55be3f4ca08f46b8
Masao KATO <ponkore@gmail.com>
2014-06-13 19:58:46 +0900

    savehist size reduced (10000->500)

--- inits/03_basic-config.el
+++ inits/03_basic-config.el
@@ -85,11 +85,11 @@
 
 ;;; 履歴
 ;; 履歴数を大きくする
-(setq history-length 10000)
+(setq history-length 500)
 ;; ミニバッファの履歴を保存する
 (savehist-mode 1)
 ;; 最近開いたファイルを保存する数を増やす
-(setq recentf-max-saved-items 10000)
+(setq recentf-max-saved-items 500)
 
 
 ;;; 圧縮
