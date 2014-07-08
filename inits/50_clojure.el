03c03f9367c6ceebe02861df04e42de45cce0925
Masao KATO <ponkore@gmail.com>
2014-07-09 00:38:57 +0900

    initialize step speed up.

--- inits/50_clojure.el
+++ inits/50_clojure.el
@@ -1,34 +1,34 @@
-(require 'cider)
-(require 'clojure-mode)
-(require 'midje-mode)
-(require 'smartparens)
-
 (add-to-list 'auto-mode-alist '("\\(default\\|user\\|emacs\\)\.\\(behaviors\\|keymap\\)" . clojure-mode))
 
-(setq cider-popup-stacktraces t)
-(setq cider-repl-popup-stacktraces t)
-(setq cider-auto-select-error-buffer t)
-(setq cider-repl-print-length 50)       ; the default is nil, no limit
-(setq cider-repl-result-prefix ";; => ")
+(eval-after-load "clojure-mode"
+  '(progn
+     (require 'smartparens)
+     (add-hook 'clojure-mode-hook 'paredit-mode)
+     (define-clojure-indent
+       (defroutes 'defun)
+       (tabular 'defun)
+       (GET 2)
+       (POST 2)
+       (PUT 2)
+       (DELETE 2)
+       (HEAD 2)
+       (ANY 2)
+       (context 2))
+     ))
 
-(define-clojure-indent
-  (defroutes 'defun)
-  (tabular 'defun)
-  (GET 2)
-  (POST 2)
-  (PUT 2)
-  (DELETE 2)
-  (HEAD 2)
-  (ANY 2)
-  (context 2))
-
-(add-hook 'clojure-mode-hook 'paredit-mode)
-
-(add-hook 'cider-mode-hook (lambda ()
-                               (auto-complete-mode)
-                               (ac-cider-compliment-setup)
-                               (smartparens-strict-mode)
-                               (yas-minor-mode)))
-(add-hook 'cider-repl-mode-hook (lambda ()
+(eval-after-load "cider"
+  '(progn
+     (add-hook 'cider-mode-hook (lambda ()
                                   (auto-complete-mode)
-                                  (ac-cider-compliment-repl-setup)))
+                                  (ac-cider-compliment-setup)
+                                  (smartparens-strict-mode)
+                                  (yas-minor-mode)))
+     (add-hook 'cider-repl-mode-hook (lambda ()
+                                       (auto-complete-mode)
+                                       (ac-cider-compliment-repl-setup)))
+     (setq cider-popup-stacktraces t)
+     (setq cider-repl-popup-stacktraces t)
+     (setq cider-auto-select-error-buffer t)
+     (setq cider-repl-print-length 50)       ; the default is nil, no limit
+     (setq cider-repl-result-prefix ";; => ")
+     (setq nrepl-sync-request-timeout 20)))
--- inits/80_helm-hosen.el
+++ inits/80_helm-hosen.el
@@ -7,23 +7,40 @@
 (defun make-helm-hosen-source-from-file (source-name filename)
   (make-helm-source-from-file source-name (concat helm-hosen-tools-dir "/" filename) 'helm-action-copy-selected))
 
-(defvar helm-root-user-zai-source          (make-helm-hosen-source-from-file "rootユーザ(在)" "helm-root-user-zai.txt"))
-(defvar helm-root-user-kan-source          (make-helm-hosen-source-from-file "rootユーザ(幹)" "helm-root-user-kan.txt"))
-(defvar helm-senmei-source                 (make-helm-hosen-source-from-file "線名(在)"       "helm-線名.txt"))
-(defvar helm-eki-zai-source                (make-helm-hosen-source-from-file "駅(在)"         "helm-駅.txt"))
-(defvar helm-kanrishitsu-eki-zai-source    (make-helm-hosen-source-from-file "管理室＿駅(在)" "helm-管理室駅.txt"))
-(defvar helm-senmei-eki-zai-source         (make-helm-hosen-source-from-file "線名＿駅(在)"   "helm-線名-駅.txt"))
-(defvar helm-kanrishitsu-kyokai-zai-source (make-helm-hosen-source-from-file "管理室境界(在)" "helm-管理室境界.txt"))
-(defvar helm-ronbutsu-henkan-source        (make-helm-hosen-source-from-file "論物変換"       "helm-dict.txt"))
+(defvar helm-hosen-initialized nil)
+
+(defvar helm-root-user-zai-source          nil)
+(defvar helm-root-user-kan-source          nil)
+(defvar helm-senmei-source                 nil)
+(defvar helm-eki-zai-source                nil)
+(defvar helm-kanrishitsu-eki-zai-source    nil)
+(defvar helm-senmei-eki-zai-source         nil)
+(defvar helm-kanrishitsu-kyokai-zai-source nil)
+(defvar helm-ronbutsu-henkan-source        nil)
+
+(defun helm-hosen-initialize
+  ()
+  (unless helm-hosen-initialized
+    (setq helm-root-user-zai-source          (make-helm-hosen-source-from-file "rootユーザ(在)" "helm-root-user-zai.txt"))
+    (setq helm-root-user-kan-source          (make-helm-hosen-source-from-file "rootユーザ(幹)" "helm-root-user-kan.txt"))
+    (setq helm-senmei-source                 (make-helm-hosen-source-from-file "線名(在)"       "helm-線名.txt"))
+    (setq helm-eki-zai-source                (make-helm-hosen-source-from-file "駅(在)"         "helm-駅.txt"))
+    (setq helm-kanrishitsu-eki-zai-source    (make-helm-hosen-source-from-file "管理室＿駅(在)" "helm-管理室駅.txt"))
+    (setq helm-senmei-eki-zai-source         (make-helm-hosen-source-from-file "線名＿駅(在)"   "helm-線名-駅.txt"))
+    (setq helm-kanrishitsu-kyokai-zai-source (make-helm-hosen-source-from-file "管理室境界(在)" "helm-管理室境界.txt"))
+    (setq helm-ronbutsu-henkan-source        (make-helm-hosen-source-from-file "論物変換"       "helm-dict.txt"))
+    (setq helm-hosen-initialized t)))
 
 (defun helm-root-user ()
   (interactive)
+  (helm-hosen-initialize)
   (helm-other-buffer
    '(helm-root-user-zai-source helm-root-user-kan-source)
    "*helm root user*"))
 
 (defun helm-hosen-zai ()
   (interactive)
+  (helm-hosen-initialize)
   (helm-other-buffer
    '(helm-senmei-source
      helm-eki-zai-source
@@ -34,6 +51,7 @@
 
 (defun helm-ronbutsu-henkan ()
   (interactive)
+  (helm-hosen-initialize)
   (helm-other-buffer
    '(helm-ronbutsu-henkan-source)
    "*helm ronbutsu henkan*"))
