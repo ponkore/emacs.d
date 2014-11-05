a4c1ea11f00fb6eab2dd75eaf79cd7e65a4b1e28
Masao KATO <ponkore@gmail.com>
2014-11-05 14:53:48 +0900

    try helm-yas

--- inits/50_yas.el
+++ inits/50_yas.el
@@ -19,3 +19,9 @@
       (signal 'quit "user quit!"))))
 
 (custom-set-variables '(yas/prompt-functions '(my-yas/prompt)))
+
+(require 'helm-c-yasnippet)
+(setq helm-yas-space-match-any-greedy t) ;[default: nil]
+(global-set-key (kbd "C-c y") 'helm-yas-complete)
+;; (yas-global-mode 1)
+;; (yas-load-directory "<path>/<to>/snippets/")
