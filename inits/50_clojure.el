ebc473487c9a63199e56723a294838155b3e0c1f
ponkore <ponkore@gmail.com>
2014-11-05 23:40:14 +0900

    midje-mode removed refactoring

--- inits/50_clojure.el
+++ inits/50_clojure.el
@@ -3,11 +3,9 @@
 (eval-after-load "clojure-mode"
   '(progn
      (require 'smartparens)
-     (require 'midje-mode)
-     (require 'midje-test-mode)
-     (add-hook 'clojure-mode-hook (lambda ()
-                                    (paredit-mode)
-                                    (yas-minor-mode)))
+     (add-hook 'clojure-mode-hook 'paredit-mode)
+     (add-hook 'clojure-mode-hook 'yas-minor-mode)
+     (add-hook 'clojure-mode-hook 'smartparens-strict-mode)
      (define-clojure-indent
        (defroutes 'defun)
        (tabular 'defun)
@@ -21,16 +19,20 @@
 
 (eval-after-load "cider"
   '(progn
-     (add-hook 'cider-mode-hook (lambda ()
-                                  (auto-complete-mode)
-                                  (ac-cider-compliment-setup)
-                                  (smartparens-strict-mode)
-                                  (yas-minor-mode)
-                                  (midje-mode)
-                                  (midje-test-mode)))
-     (add-hook 'cider-repl-mode-hook (lambda ()
-                                       (auto-complete-mode)
-                                       (ac-cider-compliment-repl-setup)))
+     (require 'ac-cider)
+     ;;(add-hook 'cider-mode-hook 'ac-flyspell-workaround)
+     (add-hook 'cider-mode-hook 'ac-cider-setup)
+
+     (add-hook 'cider-repl-mode-hook 'ac-cider-setup)
+     (eval-after-load "auto-complete"
+       '(add-to-list 'ac-modes 'cider-mode))
+
+     (defun set-auto-complete-as-completion-at-point-function ()
+       (setq completion-at-point-functions '(auto-complete)))
+     (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+
+     (add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
+     (add-hook 'cider-repl-mode-hook 'ac-cider-setup)
      (setq cider-show-error-buffer t)
      (setq cider-auto-select-error-buffer t)
      (setq cider-repl-print-length 50)  ; the default is nil, no limit
