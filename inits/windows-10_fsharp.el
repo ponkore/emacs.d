c516c02975a475739ab12108c6326f7638b23ac2
Masao KATO <ponkore@gmail.com>
2015-05-25 21:55:22 +0900

    force process coding system to cp932

--- inits/windows-10_fsharp.el
+++ inits/windows-10_fsharp.el
@@ -2,3 +2,10 @@
 
 (setq inferior-fsharp-program "\"C:\\Program Files\\Microsoft F#\\v4.0\\Fsi.exe\"")
 (setq fsharp-compiler "\"C:\\Program Files\\Microsoft F#\\v4.0\\Fsc.exe\"")
+
+(defun inferior-fsharp-mode-hook-fn ()
+  ""
+  ;; (setq comint-output-filter-functions 'comint-truncate-buffer)
+  (set-buffer-process-coding-system 'cp932 'cp932))
+
+(add-hook 'inferior-fsharp-mode-hooks 'inferior-fsharp-mode-hook-fn)
