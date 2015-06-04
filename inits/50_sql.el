b0736675fc1abe3961b4c0658491c92e5415ba59
Masao KATO <ponkore@gmail.com>
2015-06-04 13:15:31 +0900

    SQL*Plus setting improve.

--- inits/50_sql.el
+++ inits/50_sql.el
@@ -19,7 +19,21 @@
 (defun sql-set-sqli-oracle-hook ()
   ""
   (setq comint-output-filter-functions 'comint-truncate-buffer)
-  (when (eq system-type 'windows-nt) (set-buffer-process-coding-system 'cp932 'cp932)))
+  (toggle-truncate-lines t)
+  (when (eq system-type 'windows-nt) (set-buffer-process-coding-system 'cp932 'cp932))
+  (comint-send-string (get-buffer-process (current-buffer)) "
+ALTER SESSION SET NLS_DATE_FORMAT='YYYY/MM/DD'
+/
+set linesize 1000
+set trimspool on
+set timing on
+set time on
+set pagesize 1000
+")
+  )
 
 (add-hook 'sql-interactive-mode-hook 'sql-set-sqli-oracle-hook)
 (add-hook 'sql-mode-hook (lambda () (yas-minor-mode-on)))
+
+;; only for my office environment
+(load (expand-file-name "~/.emacs.d/config-sqlplus.el") t)
