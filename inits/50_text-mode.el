b636d3a78d3b3b1ec2ab44727efcab8d4df62ca4
Masao KATO <ponkore@gmail.com>
2015-11-06 23:43:52 +0900

    .table.txt setting removed, indent-tabs-mode setting added.

--- inits/50_text-mode.el
+++ inits/50_text-mode.el
@@ -7,9 +7,4 @@
  'text-mode-hook
  (lambda ()
    ;; スペースでインデントする
-   (setq indent-tabs-mode nil)
-   ;; .table.txt の場合は truncate-line する
-   (when (and (buffer-file-name)
-              (string= (substring-no-properties (buffer-file-name) -10) ".table.txt"))
-     (toggle-truncate-lines 1)
-     (setq buffer-read-only t))))
+   (setq indent-tabs-mode nil)))
