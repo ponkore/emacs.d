c5364c21ab237c57a48ac0ee43c7a9583e5924c6
Masao KATO <ponkore@gmail.com>
2015-11-06 23:40:02 +0900

    C# indent setting modified

--- inits/50_csharp-mode.el
+++ inits/50_csharp-mode.el
@@ -9,3 +9,14 @@
 
 (add-hook 'csharp-mode-hook 'my-csharp-mode-fn t)
 
+;; http://ongaeshi.hatenablog.com/entry/20110116/1295187496
+(add-hook 'csharp-mode-hook
+          '(lambda()
+             (setq comment-column 40)
+             (setq c-basic-offset 4)
+             ;; (font-lock-add-magic-number)
+             ;; オフセットの調整
+             (c-set-offset 'substatement-open 0)
+             (c-set-offset 'case-label '+)
+             (c-set-offset 'arglist-intro '+)
+             (c-set-offset 'arglist-close 0)))
