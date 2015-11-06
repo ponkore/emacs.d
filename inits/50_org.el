a04429a30e2b246d7e066aae12c616f5c3442a36
Masao KATO <ponkore@gmail.com>
2015-11-06 23:42:06 +0900

    remove empty size file when file saved.

--- inits/50_org.el
+++ inits/50_org.el
@@ -9,3 +9,11 @@
 
 (setq org-todo-keywords
       '((sequence "TODO" "FEEDBACK" "VERIFY" "|" "DONE" "DELEGATED")))
+
+;; いちいち消すのも面倒なので、内容が 0 ならファイルごと削除する
+(defun delete-file-if-no-contents ()
+  (let ((file (buffer-file-name (current-buffer))))
+    (when (= (point-min) (point-max))
+      (delete-file file)
+      (message (concat "File: " file " deleted.")))))
+(add-hook 'after-save-hook 'delete-file-if-no-contents)
