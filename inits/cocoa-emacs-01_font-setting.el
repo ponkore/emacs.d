6ff58657ef6fdf6b69ef77ce177af0602fc5446d
KatoMasao <masao@ncap9418.local>
2016-03-06 21:02:29 +0900

    change default font Menlo+Hiragino to Ricty

--- inits/cocoa-emacs-01_font-setting.el
+++ inits/cocoa-emacs-01_font-setting.el
@@ -1,47 +1,23 @@
 ;;;
 (defun cocoa-emacs-font-setting (size)
-  "Set cocoa emacs japanese fonts."
-  (let* ((asciifont "Menlo")
-         (jpfont "Hiragino Maru Gothic ProN")
+  "Set cocoa emacs japanese fonts(Ricty Diminished Version)."
+  (let* ((asciifont "Ricty Diminished")
+         (jpfont "Ricty Diminished")
          (h (* size 10))
          (fontspec)
          (jp-fontspec))
     (set-face-attribute 'default nil :family asciifont :height h)
     (setq fontspec (font-spec :family asciifont))
     (setq jp-fontspec (font-spec :family jpfont))
+    (set-fontset-font nil 'japanese-jisx0201 jp-fontspec)
     (set-fontset-font nil 'japanese-jisx0208 jp-fontspec)
     (set-fontset-font nil 'japanese-jisx0212 jp-fontspec)
     (set-fontset-font nil 'japanese-jisx0213-1 jp-fontspec)
     (set-fontset-font nil 'japanese-jisx0213-2 jp-fontspec)
+    (set-fontset-font nil 'japanese-jisx0213.2004-1 jp-fontspec)
     (set-fontset-font nil 'katakana-jisx0201 jp-fontspec)
     (set-fontset-font nil '(#x0080 . #x024F) fontspec)
-    (set-fontset-font nil '(#x0370 . #x03FF) fontspec)))
+    (set-fontset-font nil '(#x0370 . #x03FF) fontspec)
+    (add-to-list 'face-font-rescale-alist '(".*Ricty.*" . 1.2))))
 
-(cocoa-emacs-font-setting 12)
-
-;; old version
-(defun cocoa-emacs-font-setting-old-version (size)
-  "Set cocoa emacs japanese fonts(old-version)."
-  (set-face-attribute 'default nil
-                      :family "monaco"
-                      :height 140)
-  (set-fontset-font
-   (frame-parameter nil 'font)
-   'japanese-jisx0208
-   '("Hiragino Maru Gothic Pro" . "iso10646-1"))
-  (set-fontset-font
-   (frame-parameter nil 'font)
-   'japanese-jisx0212
-   '("Hiragino Maru Gothic Pro" . "iso10646-1"))
-  (set-fontset-font
-   (frame-parameter nil 'font)
-   'mule-unicode-0100-24ff
-   '("monaco" . "iso10646-1"))
-  (setq face-font-rescale-alist
-        '(("^-apple-hiragino.*" . 1.2)
-          (".*osaka-bold.*" . 1.2)
-          (".*osaka-medium.*" . 1.2)
-          (".*courier-bold-.*-mac-roman" . 1.0)
-          (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
-          (".*monaco-bold-.*-mac-roman" . 0.9)
-          ("-cdac$" . 1.3))))
+(cocoa-emacs-font-setting 14)
