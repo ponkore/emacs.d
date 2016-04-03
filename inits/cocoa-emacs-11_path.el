2b80473fd9a1a44dec5b82e741977d6938169281
ponkore <ponkore@gmail.com>
2016-04-03 18:03:01 +0900

    add eaw, exec-path fix for OSX

--- inits/cocoa-emacs-11_path.el
+++ inits/cocoa-emacs-11_path.el
@@ -6,7 +6,7 @@
 
 This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
   (interactive)
-  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "/bin/bash --login -i -c 'echo $PATH'"))))
     (setenv "PATH" path-from-shell)
     (setq exec-path (split-string path-from-shell path-separator))))
 
