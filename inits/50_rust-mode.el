7e4a215e71b22c9e3cf88b63f27c8567636420e7
ponkore <ponkore@gmail.com>
2017-01-27 21:39:44 +0900

    company-racer, flycheck, etc.

--- inits/50_rust-mode.el
+++ inits/50_rust-mode.el
@@ -3,8 +3,13 @@
 ;;
 (setq racer-cmd (expand-file-name "~/.cargo/bin/racer"))
 (setq racer-rust-src-path (expand-file-name "~/.rust-src/src/"))
-(add-hook 'rust-mode-hook #'racer-mode)
-(add-hook 'racer-mode-hook #'eldoc-mode)
-(add-hook 'racer-mode-hook #'company-mode)
+(add-hook 'rust-mode-hook (lambda ()
+                            (racer-mode)
+                            (flycheck-mode)))
+(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+(add-hook 'racer-mode-hook (lambda ()
+                             (company-mode)
+                             (eldoc-mode)))
 (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
 (setq company-tooltip-align-annotations t)
+
