79cdb7396778bc092731f59d05c528c77ce2fe81
Masao KATO <ponkore@gmail.com>
2017-09-20 13:22:35 +0900

    go/win fix, use-package for sql-mode

--- inits/50_sql.el
+++ inits/50_sql.el
@@ -1,32 +1,36 @@
-;;; for SQL mode (My Office PC Oracle setting)
-(when (eq system-type 'windows-nt)
-  (setq sql-oracle-program "c:/Apps/Oracle/sqlplus.exe")
-  ;; 新規作成のときだけ cp932 にする
-  (add-hook 'sql-mode-hook (lambda ()
-                             (unless (file-exists-p (buffer-file-name (current-buffer)))
-                               (set-buffer-file-coding-system 'cp932)
-                               (set-buffer-modified-p nil)))))
+(use-package sql-mode
+  :defer t
+  :mode (("\\.ddl$" . sql-mode))
+  :init
+  ;; for SQL mode (My Office PC Oracle setting)
+  (when (eq system-type 'windows-nt)
+    (setq sql-oracle-program "c:/Apps/Oracle/sqlplus.exe")
+    ;; 新規作成のときだけ cp932 にする
+    (add-hook 'sql-mode-hook (lambda ()
+                               (unless (file-exists-p (buffer-file-name (current-buffer)))
+                                 (set-buffer-file-coding-system 'cp932)
+                                 (set-buffer-modified-p nil)))))
 
-(when (or (eq system-type 'berkeley-unix) (eq system-type 'darwin))
-  (let ((oracle-home (expand-file-name "~/Applications/Oracle/instantclient_10_2")))
-    (setenv "NLS_LANG" "JAPANESE_JAPAN.UTF8")
-    (setenv "DYLD_LIBRARY_PATH" oracle-home)
-    (setenv "LD_LIBRARY_PATH" oracle-home)
-    (setq sql-oracle-program (concat oracle-home "/sqlplus"))))
+  ;; on Mac, set environment variables
+  (when (or (eq system-type 'berkeley-unix) (eq system-type 'darwin))
+    (let ((oracle-home (expand-file-name "~/Applications/Oracle/instantclient_10_2")))
+      (setenv "NLS_LANG" "JAPANESE_JAPAN.UTF8")
+      (setenv "DYLD_LIBRARY_PATH" oracle-home)
+      (setenv "LD_LIBRARY_PATH" oracle-home)
+      (setq sql-oracle-program (concat oracle-home "/sqlplus"))))
 
-;;; set Oracle as default SQL product.
-(setq sql-product 'oracle)
-(add-to-list 'auto-mode-alist '("\\.ddl$" . sql-mode))
+  ;; set Oracle as default SQL product.
+  (setq sql-product 'oracle)
 
-;; does not work below.
-;; (when (eq system-type 'windows-nt)
-;;   (add-to-list 'process-coding-system-alist '("sqlplus" cp932 . cp932)))
-(defun sql-set-sqli-oracle-hook ()
-  ""
-  (setq comint-output-filter-functions 'comint-truncate-buffer)
-  (toggle-truncate-lines t)
-  (when (eq system-type 'windows-nt) (set-buffer-process-coding-system 'cp932 'cp932))
-  (comint-send-string (get-buffer-process (current-buffer)) "
+  ;; does not work below.
+  ;; (when (eq system-type 'windows-nt)
+  ;;   (add-to-list 'process-coding-system-alist '("sqlplus" cp932 . cp932)))
+  (defun sql-set-sqli-oracle-hook ()
+    ""
+    (setq comint-output-filter-functions 'comint-truncate-buffer)
+    (toggle-truncate-lines t)
+    (when (eq system-type 'windows-nt) (set-buffer-process-coding-system 'cp932 'cp932))
+    (comint-send-string (get-buffer-process (current-buffer)) "
 ALTER SESSION SET NLS_DATE_FORMAT='YYYY/MM/DD'
 /
 set linesize 1000
@@ -34,19 +38,20 @@ set trimspool on
 set timing on
 set pagesize 1000
 ")
+    )
+  (require 'yasnippet)
+  (add-hook 'sql-interactive-mode-hook 'sql-set-sqli-oracle-hook)
+  (add-hook 'sql-mode-hook (lambda ()
+                             (yas-minor-mode-on)
+                             (setq indent-tabs-mode nil)
+                             (define-key sql-mode-map (kbd "C-c \"") 'wrap-double-quote-thing-at-symbol)
+                             (define-key sql-mode-map (kbd "C-c ,") 'move-trailing-comma-to-line-start)))
+
+  ;; only for my office environment
+  (load (expand-file-name "~/.emacs.d/config-sqlplus.el") t)
+
+  ;; customize font-lock
+  (font-lock-add-keywords 'sql-mode '(("\"\\([^\"]*\\)\"" . 'font-lock-constant-face)
+                                      ("\\<Hgs\\w+\\>\.\\<\\w+\\>" . 'font-lock-builtin-face)
+                                      ("\\<R[LSC][0-9][A-Z]\\w+\\>\.\\<\\w+\\>" . 'font-lock-builtin-face)))
   )
-
-(add-hook 'sql-interactive-mode-hook 'sql-set-sqli-oracle-hook)
-(add-hook 'sql-mode-hook (lambda ()
-                           (yas-minor-mode-on)
-                           (setq indent-tabs-mode nil)
-                           (define-key sql-mode-map (kbd "C-c \"") 'wrap-double-quote-thing-at-symbol)
-                           (define-key sql-mode-map (kbd "C-c ,") 'move-trailing-comma-to-line-start)))
-
-;; only for my office environment
-(load (expand-file-name "~/.emacs.d/config-sqlplus.el") t)
-
-;; customize font-lock
-(font-lock-add-keywords 'sql-mode '(("\"\\([^\"]*\\)\"" . 'font-lock-constant-face)
-                                    ("\\<Hgs\\w+\\>\.\\<\\w+\\>" . 'font-lock-builtin-face)
-                                    ("\\<R[LSC][0-9][A-Z]\\w+\\>\.\\<\\w+\\>" . 'font-lock-builtin-face)))
--- inits/windows-07_go.el
+++ inits/windows-07_go.el
@@ -1,10 +1,93 @@
 ;;
 ;; gocode coding system
 ;;
-(defadvice company-go--invoke-autocomplete (around company-go--invoke-autocomplete-adv activate)
-  (let ((old-default-process-coding-system default-process-coding-system))
-    (setq default-process-coding-system '(utf-8-unix . utf-8-unix))
-    ad-do-it
+(use-package company-go
+  :defer t
+  :config
+  (defadvice company-go--invoke-autocomplete (around company-go--invoke-autocomplete-adv activate)
+    (let ((old-default-process-coding-system default-process-coding-system))
+      (setq default-process-coding-system '(utf-8-unix . utf-8-unix))
+      ad-do-it
     (setq default-process-coding-system old-default-process-coding-system)))
+  (ad-activate-regexp "company-go--invoke-autocomplete-adv"))
 
-(ad-activate-regexp "company-go--invoke-autocomplete-adv")
+(use-package go-mode
+  :defer t
+  :config
+  (defun gofmt ()
+  "Format the current buffer according to the formatting tool.
+
+The tool used can be set via ‘gofmt-command` (default: gofmt) and additional
+arguments can be set as a list via ‘gofmt-args`."
+  (interactive)
+  (let ((tmpfile (make-temp-file "gofmt" nil ".go"))
+        (patchbuf (get-buffer-create "*Gofmt patch*"))
+        (errbuf (if gofmt-show-errors (get-buffer-create "*Gofmt Errors*")))
+        (coding-system-for-read 'utf-8-unix)
+        (coding-system-for-write 'utf-8-unix)
+        our-gofmt-args)
+
+    (unwind-protect
+        (save-restriction
+          (widen)
+          (if errbuf
+              (with-current-buffer errbuf
+                (setq buffer-read-only nil)
+                (erase-buffer)))
+          (with-current-buffer patchbuf
+            (erase-buffer))
+
+          (write-region nil nil tmpfile)
+
+          (when (and (gofmt--is-goimports-p) buffer-file-name)
+            (setq our-gofmt-args
+                  (append our-gofmt-args
+                          ;; srcdir, despite its name, supports
+                          ;; accepting a full path, and some features
+                          ;; of goimports rely on knowing the full
+                          ;; name.
+                          (list "-srcdir" (file-truename buffer-file-name)))))
+          (setq our-gofmt-args (append our-gofmt-args
+                                       gofmt-args
+                                       (list "-w" tmpfile)))
+          (message "Calling gofmt: %s %s" gofmt-command our-gofmt-args)
+          ;; We're using errbuf for the mixed stdout and stderr output. This
+          ;; is not an issue because gofmt -w does not produce any stdout
+          ;; output in case of success.
+          (if (zerop (apply #'call-process gofmt-command nil errbuf nil our-gofmt-args))
+              (progn
+                (if (zerop (call-process-region (point-min) (point-max) "diff" nil patchbuf nil "-n" "-" tmpfile))
+                    (message "Buffer is already gofmted")
+                  (go--apply-rcs-patch patchbuf)
+                  (message "Applied gofmt"))
+                (if errbuf (gofmt--kill-error-buffer errbuf)))
+            (message "Could not apply gofmt")
+            (if errbuf (gofmt--process-errors (buffer-file-name) tmpfile errbuf))))
+
+      (kill-buffer patchbuf)
+      (delete-file tmpfile))))
+  (defun godef--call (point)
+  "Call godef, acquiring definition position and expression
+description at POINT."
+  (if (not (buffer-file-name (go--coverage-origin-buffer)))
+      (error "Cannot use godef on a buffer without a file name")
+    (let ((outbuf (generate-new-buffer "*godef*"))
+          (coding-system-for-read 'utf-8-unix)
+          (coding-system-for-write 'utf-8-unix))
+      (prog2
+          (call-process-region (point-min)
+                               (point-max)
+                               godef-command
+                               nil
+                               outbuf
+                               nil
+                               "-i"
+                               "-t"
+                               "-f"
+                               (file-truename (buffer-file-name (go--coverage-origin-buffer)))
+                               "-o"
+                               ;; Emacs point and byte positions are 1-indexed.
+                               (number-to-string (1- (position-bytes point))))
+          (with-current-buffer outbuf
+            (split-string (buffer-substring-no-properties (point-min) (point-max)) "\n"))
+        (kill-buffer outbuf))))))
