749d2f874afa8f3fa65eb99698617f8c1e4196f9
ponkore <ponkore@gmail.com>
2018-09-24 00:20:36 +0900

    add slime, vue configuration

---
+++ inits/50_slime.el
@@ -0,0 +1,8 @@
+(use-package slime
+  :defer t
+  :config
+  (setq inferior-lisp-program "sbcl")
+  (require 'slime)
+  (slime-setup '(slime-repl slime-fancy slime-banner slime-company))
+  (define-key company-active-map (kbd "\C-d") 'company-show-doc-buffer)
+  (define-key company-active-map (kbd "M-.") 'company-show-location))
