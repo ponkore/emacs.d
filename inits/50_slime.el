522328f27e69043c32a0d41695087c3bcbe86d00
Masao KATO <masao.kato@no-sinker.co.jp>
2018-09-25 12:23:05 +0900

    update lisp-program (sbcl -> ros run)

--- inits/50_slime.el
+++ inits/50_slime.el
@@ -1,7 +1,7 @@
 (use-package slime
   :defer t
   :config
-  (setq inferior-lisp-program "sbcl")
+  (setq inferior-lisp-program "ros run")
   (require 'slime)
   (slime-setup '(slime-repl slime-fancy slime-banner slime-company))
   (define-key company-active-map (kbd "\C-d") 'company-show-doc-buffer)
