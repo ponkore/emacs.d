4bb14439ce768fd351a3b53f2581e41b8a1c42ac
ponkore <ponkore@gmail.com>
2019-02-08 21:30:29 +0900

    rainbow-delimiters-mode

--- use-packages.el
+++ use-packages.el
@@ -153,17 +153,18 @@
     :group 'markdown-faces))
 
 (use-package rainbow-delimiters
+  :commands rainbow-delimiters-mode
+  :init
+  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
   :config
   (require 'cl-lib)
   (require 'color)
-  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
   ;;(global-rainbow-delimiters-mode)
   (cl-loop
    for index from 1 to rainbow-delimiters-max-face-count
    do
    (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
      (cl-callf color-saturate-name (face-foreground face) 30))))
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; text-mode
