f9d90b93e676bb2f3781ef1c1266e2b51d7fbd14
ponkore <ponkore@gmail.com>
2019-03-08 22:54:00 +0900

    01_functions-local.el merged into 00_convenience.el

--- inits/00_convenience.el
+++ inits/00_convenience.el
@@ -94,3 +94,42 @@
         (delete-file file)
         (message (concat "File: " file " deleted.")))))
 (add-hook 'after-save-hook 'delete-file-if-no-contents)
+
+;;
+;;
+;;
+
+;;; match-paren
+(defun my-match-paren (arg)
+  "Go to the matching parenthesis if on parenthesis otherwise insert %."
+  (interactive "p")
+  (cond
+   ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+   ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+   (t (self-insert-command (or arg 1)))))
+
+;;; read plain file
+(defun read-file-and-list-each-lines (filename)
+  (save-excursion
+    (let* ((buffer (find-file-noselect filename))
+           (ret nil))
+      (set-buffer buffer)
+      (goto-char (point-min))
+      (while (re-search-forward "^.+$" nil t)
+        (setq ret (cons (match-string-no-properties 0) ret)))
+      (kill-buffer buffer)
+      (reverse ret))))
+
+;; (defun read-file-and-list-each-lines (filename)
+;;   (save-excursion
+;;     (let* ((buffer (find-file-noselect filename))
+;;            (ret nil))
+;;       (with-current-buffer buffer
+;;         (setq ret (split-string (buffer-string) "[\r\n]")))
+;;       (kill-buffer buffer)
+;;       ret)))
+
+;;; insert current datetime
+(defun my-insert-datetime ()
+  (interactive)
+  (insert (format-time-string "%Y/%m/%d %T")))
