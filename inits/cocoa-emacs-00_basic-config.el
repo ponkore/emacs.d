d330c9e7d2d6e3a46e64ea1b618154f7698e2d39
ponkore <ponkore@gmail.com>
2020-01-09 09:55:16 +0900

    remove dired-hack, fsharp

--- inits/cocoa-emacs-00_basic-config.el
+++ inits/cocoa-emacs-00_basic-config.el
@@ -1,18 +1,6 @@
 ;; process-coding-system を utf-8 にする。(その他は設定不要？)
 (setq default-process-coding-system '(utf-8-unix . utf-8-unix))
 
-;; dired hack
-(defvar open-directory-command "open" "Open a directory with suitable windows/mac command.")
-(defvar open-file-command "open" "Open a file with suitable windows/mac command.")
-(defun dired-open-external ()
-  "Open current line of dired buffer with external (windows) command."
-  (interactive)
-  (let ((file (dired-get-filename nil t)))
-    (if (file-directory-p file)
-        (start-process "dir" nil open-directory-command file)
-      (start-process "file" nil open-file-command file))))
-(add-hook 'dired-mode-hook (lambda () (define-key dired-mode-map " " 'dired-open-external)))
-
 (set-frame-parameter nil 'alpha 90)
 
 (setq initial-frame-alist
@@ -23,14 +11,6 @@
          (internal-border-width . 0))))
 
 (setq default-frame-alist initial-frame-alist)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; fsharp
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; F# 編集用のモード
-
-(setq inferior-fsharp-program "/usr/bin/fsharpi --readline-")
-(setq fsharp-compiler "/usr/bin/fsharpc")
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; path
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
--- inits/windows-01_config.el
+++ inits/windows-01_config.el
@@ -8,21 +8,6 @@
           (lambda () (deactivate-input-method)))
 
 ;;
-;; dired
-;;
-;; Windows dired quick hack: open any documents with external command.
-(defvar open-directory-command "start" "Open a directory with suitable windows/mac command.")
-(defvar open-file-command "start" "Open a file with suitable windows/mac command.")
-(defun dired-open-external ()
-  "Open current line of dired buffer with external (windows) command."
-  (interactive)
-  (let ((file (dired-get-filename nil t)))
-    (if (file-directory-p file)
-        (start-process "dir" nil open-directory-command file)
-      (start-process "file" nil open-file-command file))))
-(add-hook 'dired-mode-hook (lambda () (define-key dired-mode-map " " 'dired-open-external)))
-
-;;
 ;; vc
 ;;
 ;; svn log の出力は cp932
@@ -51,20 +36,9 @@
 ;; on Windows, use lein.bat instead of lein shell script.
 (setq cider-lein-command "lein.bat")
 
-;;
-;; F# 編集用のモード
-;;
-(setq inferior-fsharp-program "\"C:\\Program Files\\Microsoft F#\\v4.0\\Fsi.exe\"")
-(setq fsharp-compiler "\"C:\\Program Files\\Microsoft F#\\v4.0\\Fsc.exe\"")
-
 (defun inferior-fsharp-mode-hook-fn ()
   ""
   ;; (setq comint-output-filter-functions 'comint-truncate-buffer)
   (set-buffer-process-coding-system 'cp932 'cp932))
 
 (add-hook 'inferior-fsharp-mode-hooks 'inferior-fsharp-mode-hook-fn)
-
-;;
-;; sql
-;;
-;;(modify-coding-system-alist 'file ".*\\.sql$" 'cp932-dos)
